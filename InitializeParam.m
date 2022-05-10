feadd0b8caaac24e40061534e16b55c556642aaf
marieangestefanos <marieange.stefanos@grenoble-inp.org>
2022-05-10 22:03:39 +0200

    init param for k layers

--- InitializeParam.m
+++ InitializeParam.m
@@ -1,12 +1,32 @@
-function theta = InitializeParam(X_train, Y_train, m)
+function NetParams = InitializeParam(X_train, Y_train, hid_dim, k)
     [d, ~] = size(X_train);
     [K, ~] = size(Y_train);
     
-    W1 = randn(m, d)/sqrt(d);
-    W2 = randn(K, m)/sqrt(m);
+    nb_hid_layers = size(hid_dim, 2);
+
+    % Just checking the number of layers
+    if nb_hid_layers ~= (k-1)
+        error("Error: hidd_dim and nb of layers do not match.")
+    end
+
+    % First hidden layer param
+    W{1} = randn( hid_dim(1), d ) / sqrt( d );
+    b{1} = zeros(hid_dim(1), 1);
     
-    b1 = zeros(m, 1);
-    b2 = zeros(K, 1);
+    % From second to last hidden layer param
+    if nb_hid_layers > 1
+        for i = 1:(nb_hid_layers-1)
+            W{i+1} = randn( hid_dim(i+1), hid_dim(i) ) / sqrt(hid_dim(i));
+            b{i+1} = zeros(hid_dim(i+1), 1);
+        end
+    else
+        i = 0;
+    end
+
+    % Output layer param
+    W{end+1} = randn( K, hid_dim(i+1) ) / sqrt(hid_dim(i+1));
+    b{end+1} = zeros(K, 1);
     
-    theta = {W1, W2, b1, b2};
+    NetParams.W = W;
+    NetParams.b = b;
 end
--- lab3_script.m
+++ lab3_script.m
@@ -1,1 +1,61 @@
-a = 
+addpath("Documents/kth/dl/labs/lab3/");
+train_path = "data_batch_1.mat";
+valid_path = "data_batch_2.mat";
+test_path = "test_batch.mat";
+
+%% LOAD DATA
+[X_train, Y_train, y_train] = LoadBatch(train_path);
+[X_valid, Y_valid, y_valid] = LoadBatch(valid_path);
+[X_test, Y_test, y_test] = LoadBatch(test_path);
+
+
+%% PREPROCESS DATA
+rng(400);
+mean_train = mean(X_train, 2);
+std_train = std(X_train, 0, 2);
+
+X_train = Preprocess(X_train, mean_train, std_train);
+X_valid = Preprocess(X_valid, mean_train, std_train);
+X_test = Preprocess(X_test, mean_train, std_train);
+
+[~, n] = size(X_train);
+
+%% INIT NETWORK PARAM
+% m = 50;
+k = 3;
+hid_dim = [20 50];
+NetParams = InitializeParam(X_train, Y_train, hid_dim, k);
+% W = theta(1:2);
+% b = theta(3:4);
+% d_batch = 10;
+% end_batch = 3;
+% h = 1e-5;
+% lambda = 0;
+% eps = 1e-6;
+% n_batch = 100;
+% eta = 0.001;
+% n_epochs = 200;
+
+% %% EVAL THE NETWORK FUNCTION
+% [H, P] = EvaluateClassifier(X_train, theta);
+
+% %% COMPUTE THE COST FUNCTION
+% [J, loss] = ComputeCost(X_train, Y_train, W, b, lambda);
+
+% %% COMPUTE THE GRADIENTS
+% X_batch = X_train(1:d_batch, 1:end_batch);
+% Y_batch = Y_train(:, 1:end_batch);
+% theta_batch = InitializeParam(X_batch, Y_batch, m);
+% W_batch = theta_batch(1:2);
+% b_batch = theta_batch(3:4);
+
+% %% GRADIENTS COMPARISONS
+% [H_batch, P_batch] = EvaluateClassifier(X_batch, theta_batch);
+% [grad_W_an, grad_b_an] = ComputeGradients(X_batch, Y_batch, H_batch, P_batch, theta_batch, lambda);
+
+% % [grad_b_num_fast, grad_W_num_fast] = ComputeGradsNum(X_batch, Y_batch, W_batch, b_batch, lambda, h);
+% [grad_b_num_slow, grad_W_num_slow] = ComputeGradsNumSlow(X_batch, Y_batch, W_batch, b_batch, lambda, h);
+
+% % [grad_W_err_fast, grad_b_err_fast] = ComputeRelativeError(grad_W_an, grad_b_an, grad_W_num_fast, grad_b_num_fast, eps);
+% [grad_W_err_slow, grad_b_err_slow] = ComputeRelativeError(grad_W_an, grad_b_an, grad_W_num_slow, grad_b_num_slow, eps);
+% % [grad_W_err_given, grad_b_err_given] = ComputeRelativeError(grad_W_num_slow, grad_b_num_slow, grad_W_num_fast, grad_b_num_fast, eps);
